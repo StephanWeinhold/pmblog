3e6d94f978d877067fbcdd136991042abaabb78a
Stephan Weinhold <stephan.weinhold@gmail.com>
2019-05-31 20:23:23 +0200

    Altered post listing.

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "gatsby";
+import PostTags from "../PostTags/PostTags";
 
 class PostListing extends React.Component {
   getPostList() {
     const postList = [];
     this.props.postEdges.forEach(postEdge => {
       postList.push({
+        id: postEdge.node.id,
         path: postEdge.node.fields.slug,
         tags: postEdge.node.frontmatter.tags,
         cover: postEdge.node.frontmatter.cover,
@@ -21,11 +23,21 @@ class PostListing extends React.Component {
     const postList = this.getPostList();
     return (
       <div>
-        {/* Your post list here. */
+        {
         postList.map(post => (
-          <Link to={post.path} key={post.title}>
-            <h1>{post.title}</h1>
-          </Link>
+          <article key={post.id} class="pt4">
+            <h1><Link to={post.path} className="link black-90">{post.title}</Link></h1>
+            <nav>
+              <span class="mr2 black-50"><time datetime="2019-03-11T00:00:00.000+00:00">{post.date}</time></span>
+              <PostTags tags={post.tags} />
+            </nav>               
+            <p class="mv4">
+              {post.excerpt}
+            </p>
+            <p>
+              <Link to={post.path} className="link pv1 ph2 underline-hover bg-dark-blue white">Weiterlesen</Link>
+            </p>
+          </article>
         ))}
       </div>
     );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -32,6 +32,7 @@ export const pageQuery = graphql`
     ) {
       edges {
         node {
+          id
           fields {
             slug
             date
