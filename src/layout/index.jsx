adfa70ca15c80ae757cf315f933ce9edfd4f4e0c
Stephan Weinhold <stephan.weinhold@gmail.com>
2019-06-01 13:21:03 +0200

    Added nav element for all categories.

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -38,7 +38,8 @@ export default class MainLayout extends React.Component {
         <section class="w-100">
           <nav class="dt center w-90 w-60-ns pv3">
             <a href="/" class="link mr3 pv1 ph2 underline-hover bg-dark-blue white">Alle Artikel</a>
-            <a href="/themen" class="link mr3 pv1 ph2 underline-hover bg-gold white">#Alle Themen</a>
+            <a href="/themen" class="link mr3 pv1 ph2 underline-hover bg-gold white">#Alle Kategorien</a>
+            <a href="/themen" class="dn di-ns link mr3 pv1 ph2 underline-hover bg-green white">#Alle Themen</a>
             <a href="#" class="link mr3 pv1 ph2 underline-hover bg-red white">Newsletter</a>
             <a href="/stephan-weinhold" class="dn di-ns link mr3 pv1 ph2 underline-hover bg-light-purple white">Über mich</a>
           </nav>
