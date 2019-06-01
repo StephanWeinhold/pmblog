7b3ca52d78082ded2e80f6e850a3ae80b176d17f
Stephan Weinhold <stephan.weinhold@gmail.com>
2019-06-01 20:00:53 +0200

    Updated some config information.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,21 +1,21 @@
 const config = {
-  siteTitle: "Gatsby Advanced Starter", // Site title.
-  siteTitleShort: "GA Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "GatsbyJS Advanced Starter", // Alternative site title for SEO.
+  siteTitle: "Projektmanagementblog", // Site title.
+  siteTitleShort: "Projektmanagementblog", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: "Projektmanagementblog. Klassisch, agil, hybrid.", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://vagr9k.github.io", // Domain of your website without pathPrefix.
   pathPrefix: "/gatsby-advanced-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "A GatsbyJS stater with Advanced design in mind.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "Gedanken über Projektmanagement. Klassisch, agil, hybrid.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
-  postDefaultCategoryID: "Tech", // Default category for posts.
+  googleAnalyticsID: "UA-74431055-2", // GA tracking ID.
+  postDefaultCategoryID: "Projektmanagement", // Default category for posts.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
-  dateFormat: "DD/MM/YYYY", // Date format for display.
-  userName: "Advanced User", // Username to display in the author segment.
-  userEmail: "AdvancedUser@example.com", // Email used for RSS feed's author segment
-  userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "North Pole, Earth", // User location to display in the author segment.
+  dateFormat: "DD.MM.YYYY", // Date format for display.
+  userName: "Stephan Weinhold", // Username to display in the author segment.
+  userEmail: "stephan.weinhold@projektmanagementblog.de", // Email used for RSS feed's author segment
+  userTwitter: "StephanWeinhold", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Salzburg, Austria", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription:
     "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
