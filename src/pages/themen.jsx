034d928fd620ce483ed2ea000fc43c0080ae2e5a
Stephan Weinhold <stephan.weinhold@gmail.com>
2019-06-01 11:05:59 +0200

    Added title to tags listing page.

--- src/pages/themen.jsx
+++ src/pages/themen.jsx
@@ -12,7 +12,7 @@ class Themen extends React.Component {
     return (
       <Layout>
         <div className="index-container">
-          <Helmet title={config.siteTitle} />
+          <Helmet title={`Themen | ${config.siteTitle}`} />
           <SEO />
           <TagListing tagEdges={tagEdges} />
         </div>
