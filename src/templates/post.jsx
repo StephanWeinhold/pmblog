9a966d2f4a0c99e2d42cfd09eccadddc4f24e1fa
Stephan Weinhold <stephan.weinhold@gmail.com>
2019-06-02 20:24:19 +0200

    Removed b16 stylings.

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -8,7 +8,6 @@ import PostCategory from "../components/PostCategory/PostCategory";
 import SocialLinks from "../components/SocialLinks/SocialLinks";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
-import "./b16-tomorrow-dark.css";
 import "./post.css";
 
 export default class PostTemplate extends React.Component {
