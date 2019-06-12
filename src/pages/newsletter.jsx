f079a394ec4a90e99b7a8cb14f21baa0732f081e
Stephan Weinhold <stephan.weinhold@gmail.com>
2019-06-12 19:50:46 +0200

    Styled and updated newsletter subscription. Fixed #11

--- src/pages/newsletter.jsx
+++ src/pages/newsletter.jsx
@@ -11,25 +11,64 @@ class NewsletterPage extends Component {
         <div className="about-container">
           <Helmet title={`Newsletter abonnieren | ${config.siteTitle}`} />
           <SEO />
-          <form id="newsletter-anmeldung" name="newsletter-anmeldung" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="newsletter-anmeldung" />
-            <p>
-              <label htmlFor="vorname">Vorname</label>
-              {" "}
-              <input type="text" name="vorname" id="vorname" />
-              {" "}
-              <label htmlFor="nachname">Nachname</label>
-              {" "}
-              <input type="text" name="nachname" id="nachname" />
-            </p>
-            <p>
-              <label htmlFor="email">E-Mail Adresse</label>
-              {" "}
-              <input type="email" name="email" id="email" />
-            </p>
-            <p>
-              <button type="submit">Anmelden</button>
-            </p>
+          <h2>Versäume keinen interessanten, spannende, liebevollen, weiterbildenden, großartigen Projektmanagementartikel mehr.</h2>
+          <p>
+            Datenblahblah
+          </p>
+          <h3>Also</h3>
+          <form id="newsletter-anmeldung" 
+            name="newsletter-anmeldung" 
+            method="POST" 
+            data-netlify="true" 
+            className="black-80">
+            <div className="measure">
+              <input type="hidden" name="form-name" value="newsletter-anmeldung" />
+              <p>
+                <label htmlFor="anrede" className="f6 b db mb2">
+                  Anrede
+                  {" "}
+                  <span className="normal black-60">(optional)</span>
+                </label>
+                <div class="flex items-center mb2">
+                  <input className="mr2" type="radio" name="anrede" id="anrede-frau" value="frau" />
+                  <label for="anrede-frau" class="lh-copy">Liebe</label>
+                </div>
+                <div class="flex items-center mb2">
+                  <input className="mr2" type="radio" name="anrede" id="anrede-mann" value="frau" />
+                  <label for="anrede-mann" class="lh-copy">Lieber</label>
+                </div>
+                <small id="anrede-desc" className="f6 black-60 db mb2">
+                  Hier könnt Ihr angeben, welches Wort in der Anrede vor Eurem Namen kommen soll. Falls Ihr denn überhaupt einen Namen angeben wollt.
+                </small>
+              </p>
+              <p>
+                <label htmlFor="vorname" className="f6 b db mb2">
+                  Vorname
+                  {" "}
+                  <span className="normal black-60">(optional)</span>
+                </label>
+                <input type="text" name="vorname" id="vorname" 
+                  className="input-reset ba b--black-20 pa2 mb2 db w-100"
+                  aria-describedby="vorname-desc" />
+                <small id="vorname-desc" className="f6 black-60 db mb2">
+                  Wenn Ihr eine persönliche Anrede im Newsletter bevorzugt, könnt Ihr hier Euren Vornamen angeben. Bitte wählt dann auch oben eine Anrede aus.
+                </small>
+              </p>
+              <p>
+                <label htmlFor="email" className="f6 b db mb2">
+                  E-Mail Adresse
+                </label>
+                <input type="text" name="email" id="email" 
+                  className="input-reset ba b--black-20 pa2 mb2 db w-100"
+                  aria-describedby="email-desc" />
+                <small id="email-desc" className="f6 black-60 db mb2">
+                  Bitte gebt mir die E-Mail Adresse bekannt, an die ich den Newsletter schicken darf.
+                </small>
+              </p>
+              <p>
+                <button className="f6 link dim ph3 pv2 mb2 ba bw0 dib white bg-dark-blue" type="submit">Anmelden</button>
+              </p>
+            </div>
           </form>
         </div>
       </Layout>
