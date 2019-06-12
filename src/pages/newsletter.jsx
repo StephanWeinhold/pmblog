990a6ba030652b7d85255c1debf8184d7dfb8320
Stephan Weinhold <stephan.weinhold@gmail.com>
2019-06-12 20:10:23 +0200

    Fixed some styling issues.

--- src/pages/newsletter.jsx
+++ src/pages/newsletter.jsx
@@ -16,12 +16,14 @@ class NewsletterPage extends Component {
             Datenblahblah
           </p>
           <h3>Also</h3>
-          <form id="newsletter-anmeldung" 
+          <form 
+            id="newsletter-anmeldung" 
             name="newsletter-anmeldung" 
             method="POST" 
             data-netlify="true" 
             action="/newsletter-angemeldet" 
-            className="black-80">
+            className="black-80"
+          >
             <div className="measure">
               <input type="hidden" name="form-name" value="newsletter-anmeldung" />
               <p>
@@ -30,13 +32,13 @@ class NewsletterPage extends Component {
                   {" "}
                   <span className="normal black-60">(optional)</span>
                 </label>
-                <div class="flex items-center mb2">
+                <div className="flex items-center mb2">
                   <input className="mr2" type="radio" name="anrede" id="anrede-frau" value="frau" />
-                  <label for="anrede-frau" class="lh-copy">Liebe</label>
+                  <label for="anrede-frau" className="lh-copy">Liebe</label>
                 </div>
-                <div class="flex items-center mb2">
+                <div className="flex items-center mb2">
                   <input className="mr2" type="radio" name="anrede" id="anrede-mann" value="frau" />
-                  <label for="anrede-mann" class="lh-copy">Lieber</label>
+                  <label for="anrede-mann" className="lh-copy">Lieber</label>
                 </div>
                 <small id="anrede-desc" className="f6 black-60 db mb2">
                   Hier könnt Ihr angeben, welches Wort in der Anrede vor Eurem Namen kommen soll. Falls Ihr denn überhaupt einen Namen angeben wollt.
@@ -48,9 +50,13 @@ class NewsletterPage extends Component {
                   {" "}
                   <span className="normal black-60">(optional)</span>
                 </label>
-                <input type="text" name="vorname" id="vorname" 
+                <input 
+                  type="text" 
+                  name="vorname" 
+                  id="vorname" 
                   className="input-reset ba b--black-20 pa2 mb2 db w-100"
-                  aria-describedby="vorname-desc" />
+                  aria-describedby="vorname-desc" 
+                />
                 <small id="vorname-desc" className="f6 black-60 db mb2">
                   Wenn Ihr eine persönliche Anrede im Newsletter bevorzugt, könnt Ihr hier Euren Vornamen angeben. Bitte wählt dann auch oben eine Anrede aus.
                 </small>
