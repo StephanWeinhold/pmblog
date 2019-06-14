c01f0983ddcc406a7ee9038553c611fb55e1a2bf
Stephan Weinhold <stephan.weinhold@gmail.com>
2019-06-14 20:43:21 +0200

    Fixed typo.

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -74,7 +74,7 @@ export default class MainLayout extends React.Component {
                   Du willst keinen Artikel mehr verpassen? Hier kannst Du Dich für meinen Newsletter anmelden.
                 </span>
               </p>
-              <input name="m" id="email-footer" placeholder="E-Mail Adresse" className="mw-100 w-100 w5-ns f5 input-reset ba b--black-20 pv3 ph4 border-box" />
+              <input name="m" id="email-footer" placeholder="E-Mailadresse" className="mw-100 w-100 w5-ns f5 input-reset ba b--black-20 pv3 ph4 border-box" />
               <input type="submit" className="input-reset w-100 w-auto-ns bg-black-80 white f5 pv2 pv3-ns ph4 ba b--black-80 bg-hover-mid-gray" />
             </form>
           </div>
