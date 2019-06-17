7887b4208fc986fe339e2f4108d6c6f593d05cd6
Stephan Weinhold <stephan.weinhold@gmail.com>
2019-06-17 19:50:19 +0200

    Lint prefer-const.

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,5 @@
 export function replaceUmlauts(stringWithUmlauts) {
-  let stringWithoutUmlauts = stringWithUmlauts.replace('ä', 'ae').replace('ö', 'oe').replace('ü', 'ue').replace('ß', 'ss');
+  const stringWithoutUmlauts = stringWithUmlauts.replace('ä', 'ae').replace('ö', 'oe').replace('ü', 'ue').replace('ß', 'ss');
   
   return stringWithoutUmlauts;
 }
