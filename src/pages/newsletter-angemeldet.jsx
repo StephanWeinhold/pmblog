9a363538541a786dda17445efa825901dfc14261
Stephan Weinhold <stephan.weinhold@gmail.com>
2019-06-30 16:32:41 +0200

    Added mailgun functionality.

--- src/pages/newsletter-angemeldet.jsx
+++ src/pages/newsletter-angemeldet.jsx
@@ -4,6 +4,29 @@ import Layout from "../layout";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 
+const API_KEY = '03454650ec1e123420afef94d321b87f-b892f62e-23304395';
+const DOMAIN = 'mail.projektmanagementblog.de';
+const HOST = 'https://api.eu.mailgun.net﻿';
+const mailgun = require('mailgun.js');
+const mg = mailgun.client(
+  {
+    url: HOST,
+    domain: DOMAIN,
+    username: 'api', 
+    key: process.env.MAILGUN_API_KEY || API_KEY,
+  }
+);
+
+mg.messages.create(DOMAIN, {
+    from: "Projektmanagementblog Newsletter <newsletter@" + DOMAIN + ">",
+    to: ["stephan.weinhold@gmail.com"],
+    subject: "Hello",
+    text: "Testing some Mailgun awesomness from the EU again!",
+    html: "<h1>Testing some Mailgun awesomness from the EU again!</h1><p>Yay yay yay.</p>"
+  })
+  .then(msg => console.log(msg)) // logs response data
+  .catch(err => console.log(err)); // logs any error
+
 class NewsletterAngemeldetPage extends Component {
   render() {
     return (
