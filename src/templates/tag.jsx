f23b1236a4d808a21714c11c2be3e551ead5f542
Stephan Weinhold <stephan.weinhold@gmail.com>
2020-02-10 19:42:44 +0100

    Fixed lint issues inside templates.

--- src/templates/tag.jsx
+++ src/templates/tag.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
-import { graphql } from "gatsby";
-import { Link } from "gatsby";
+import { graphql, Link } from "gatsby";
 import Layout from "../layout";
 import PostListing from "../components/PostListing/PostListing";
 import config from "../../data/SiteConfig";
